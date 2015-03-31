4760814d78aced97141f8184dd6808d78ea10f09
Nick Spinale <spinalen@carleton.edu>
2015-03-31 16:06:00 -0500

    now using builders

--- src/Data/Aencode.hs
+++ src/Data/Aencode.hs
@@ -2,27 +2,25 @@
 
 module Data.Aencode
     (
-  --
       BValue(..)
-    , ToBencode
-    , FromBencode
-    , ToBencode'
-    , FromBencode'
-  --
-    , _BValue
-    , _Translated
-    , _Translated'
-  --
     , _BString
     , _BInt
     , _BList
     , _BDict
   --
-    , onlyDo
     , parseBValue
+    , onlyDo
+    , onlyDo'
   --
     , writeBValue
   --
+    , ToBencode
+    , FromBencode
+    , ToBencode'
+    , FromBencode'
+    , _BValue
+    , _Translated
+    , _Translated'
     ) where
 
 import           Control.Applicative
@@ -30,8 +28,12 @@ import           Control.Lens
 import           Control.Monad
 import           Data.Attoparsec.ByteString
 import           Data.Attoparsec.ByteString.Char8 (char, decimal, signed)
+import qualified Data.Attoparsec.ByteString.Lazy as A
 import qualified Data.ByteString as B
+import           Data.ByteString.Builder
 import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as L
+import           Data.Monoid
 import           Data.Function
 import qualified Data.Map as M
 import           Prelude hiding (take)
@@ -43,24 +45,9 @@ data BValue = BString B.ByteString
             | BDict (M.Map B.ByteString BValue)
             deriving Show
 
-class ToBencode a where
-    encode :: a -> BValue
-
-class FromBencode a where
-    decode :: BValue -> Maybe a
-
-class ToBencode' a where
-    encode' :: c -> a -> BValue
-
-class FromBencode' a where
-    decode' :: c -> BValue -> Maybe a
-
 ----------------------------------------
 -- PARSERS
 ----------------------------------------
-
-onlyDo :: Parser a -> B.ByteString -> Maybe a
-onlyDo = ((maybeResult . (`feed` B.empty)) .) . parse . (<* endOfInput)
 
 -- Parse a Bencoded value
 parseBValue :: Parser BValue
@@ -80,51 +67,73 @@ parseBList = char 'l' *> many' parseBValue <* char 'e'
 
 parseBDict :: Parser (M.Map B.ByteString BValue)
 parseBDict = char 'd' *> inner <* char 'e'
+  where
+    inner = do
+        pairs <- many' $ (,) <$> parseBString <*> parseBValue
+        if sorted pairs
+         then return (M.fromAscList pairs)
+         else empty
+    sorted x@(_:y) = all (== LT) $ zipWith (compare `on` fst) x y
+    sorted _ = True
 
-inner = do
-    pairs <- many' $ liftA2 (,) parseBString parseBValue
-    if sorted pairs
-     then return (M.fromAscList pairs)
-     else empty
+-- Not very efficient because toStrict's a bytestring
+_BValue :: Prism' B.ByteString BValue
+_BValue = prism' (L.toStrict . toLazyByteString . writeBValue) (onlyDo parseBValue)
 
-sorted :: Ord a => [(a, b)] -> Bool
-sorted x@(_:y) = all (== LT) $ zipWith (compare `on` fst) x y
-sorted _ = True
+_BValue' :: Prism' L.ByteString BValue
+_BValue' = prism' (toLazyByteString . writeBValue) (onlyDo' parseBValue)
+
+-- Parse exactly a _ (strict)
+onlyDo :: Parser a -> B.ByteString -> Maybe a
+onlyDo = ((maybeResult . (`feed` B.empty)) .) . parse . (<* endOfInput)
+
+-- Parse exactly a _ (lazy)
+onlyDo' :: Parser a -> L.ByteString -> Maybe a
+onlyDo' = (A.maybeResult .) . A.parse . (<* endOfInput)
 
 ----------------------------------------
 -- WRITERS
 ----------------------------------------
 
-writeBValue :: BValue -> B.ByteString
+writeBValue :: BValue -> Builder
 writeBValue (BString x) = writeBString x
 writeBValue (BInt    x) = writeBInt    x
 writeBValue (BList   x) = writeBList   x
 writeBValue (BDict   x) = writeBDict   x
 
-writeBString :: B.ByteString -> B.ByteString
-writeBString s = C.pack (show (B.length s) ++ ":") `B.append` s
+writeBString :: B.ByteString -> Builder
+writeBString s = intDec (B.length s) <> char8 ':' <> byteString s
 
-writeBInt :: Integer -> B.ByteString
-writeBInt = surround 'i' . C.pack . show
+writeBInt :: Integer -> Builder
+writeBInt = surround 'i' . integerDec
 
-writeBList :: [BValue] -> B.ByteString
-writeBList = surround 'l' . B.concat . map writeBValue
+writeBList :: [BValue] -> Builder
+writeBList = surround 'l' . mconcat . map writeBValue
 
-writeBDict :: M.Map B.ByteString BValue -> B.ByteString
-writeBDict = surround 'd'
-           . B.concat
-           . map (\(k, v) -> writeBString k `B.append` writeBValue v)
-           . M.toAscList
+writeBDict :: M.Map B.ByteString BValue -> Builder
+writeBDict d = surround 'd' $ mconcat
+                   [ writeBString k <> writeBValue v
+                   | (k, v) <-  M.toAscList d
+                   ]
 
-surround :: Char -> B.ByteString -> B.ByteString
-surround = (.) (`C.snoc` 'e') . C.cons
+surround :: Char -> Builder -> Builder
+surround = (.) (<> char8 'e') . (<>) . char8
 
 ----------------------------------------
 -- PRISMS
 ----------------------------------------
 
-_BValue :: Prism' B.ByteString BValue
-_BValue = prism' writeBValue (onlyDo parseBValue)
+class ToBencode a where
+    encode :: a -> BValue
+
+class FromBencode a where
+    decode :: BValue -> Maybe a
+
+class ToBencode' a where
+    encode' :: c -> a -> BValue
+
+class FromBencode' a where
+    decode' :: c -> BValue -> Maybe a
 
 _Translated :: (FromBencode a, ToBencode a) => Prism' BValue a
 _Translated = prism' encode decode
